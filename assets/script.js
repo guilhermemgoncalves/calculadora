dfedf0e7cf5fb2ac6e80e9b839a657ef63916a1b
Guilherme Marciano Gonçalves <g.marciano.goncalves@avanade.com>
2022-07-12 15:48:57 -0300

    Inicio do JS

---
+++ assets/script.js
@@ -0,0 +1,55 @@
+let result = document.getElementById('displayedResult');
+
+result.innerText = '0';
+
+addEventListener("click", function(event) {
+
+
+    let target = event.target;
+   
+    if (target.value !== undefined ) {
+    result.innerHTML = target.value;
+
+
+
+     
+
+
+
+
+
+
+
+
+
+
+
+}
+
+function IsNumber()
+{
+
+}
+
+function IsOperator()
+{
+
+}
+function IsEqualsSignal()
+{
+
+}
+function IsClear()
+{
+
+}
+
+function DisplayNumber()
+{
+
+}
+
+
+
+
+});
