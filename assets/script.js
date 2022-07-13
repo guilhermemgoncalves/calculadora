220533e810253478f8cdf7a650a25dc7c0922c7f
Guilherme Marciano Goncalves <225587@SAMBA.SLT.IFSP.EDU.BR>
2022-07-12 22:48:12 -0300

    Finalizada a lógica da calculadora

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,21 @@
 let result = document.getElementById("displayedResult");
 
+const db = []
+
+const objCalculo = (firstEl,secondEL,operator)=>{
+  [PrimeiroElemento] = firstEl;
+  [SegundoElemento] = secondEL;
+  [Operador] = operator;
+
+  return `${PrimeiroElemento}, ${SegundoElemento}, ${Operador}`
+}
+
+
 result.innerText = "0";
-let resultCalc = 0;
+let displayNumber = 0;
+let currentNumber = 0;
+let currentOperator = null;
+let flagNew = true;
 
 addEventListener("click", function (event) {
   let target = event.target;
@@ -12,32 +26,97 @@ addEventListener("click", function (event) {
     } else if (target.value == "C") {
       IsClear();
     } else if (target.value == "=") {
-        IsEqualsSignal();
+      IsEqualsSignal();
+      DisplayPrevious();
+    } else {
+      IsOperator(target.value);
     }
-    else{
-        IsOperator();
-    }
   }
 
-  function IsNumber(numberClick) {
-    if (result.innerText == "0") {
-      resultCalc = numberClick;
-    } else resultCalc += numberClick;
-
-    console.log(resultCalc);
-    result.innerText = resultCalc;
-  }
-
-  function IsOperator() {
-      console.log("Cheguei");
-}
-function IsEqualsSignal() {    
-
-  }
-  function IsClear() {
-    resultCalc = 0;
-    result.innerText = resultCalc;
-  }
-
-  function DisplayNumber() {}
+  console.log(displayNumber + "  " + currentNumber + "   " + currentOperator);
 });
+
+function IsNumber(numberClick) {
+  if (flagNew) {
+    displayNumber = numberClick;
+    result.innerText = displayNumber;
+    flagNew = false;
+  } else {
+    displayNumber += numberClick;
+    result.innerText = displayNumber;
+  }
+}
+
+function IsOperator(operator) {
+  if(currentOperator==null){
+  currentOperator = operator;
+  currentNumber = displayNumber;
+  result.innerText += " " + operator;
+  displayNumber = 0;
+  flagNew = true;
+  }
+  else{
+  IsClear()
+  result.innerText = "0";
+  }
+}
+function IsEqualsSignal() {
+  let finalResult;
+
+  switch (currentOperator) {
+    case "+":
+      finalResult = Number(currentNumber) + Number(displayNumber);
+      break;
+    case "-":
+      finalResult = Number(currentNumber) - Number(displayNumber);
+      break;
+    case "*":
+      finalResult = Number(currentNumber) * Number(displayNumber);
+      break;
+    case "/":
+      finalResult = Number(currentNumber) / Number(displayNumber);
+      break;
+    case null:
+      finalResult = 0;
+
+    default:
+      break;
+  }  
+
+  console.log(`Esse é o DB: ${db}`)
+
+  
+
+  db.push({
+    primeiroEL: currentNumber,
+    segundoEl: displayNumber,
+    Operador: currentOperator,
+    Reulstado: finalResult
+  });
+
+
+
+  result.innerText = finalResult;
+  IsClear();
+
+  
+ 
+  
+  
+
+  console.log(finalResult);
+
+}
+function IsClear() {
+  displayNumber = 0;
+  currentNumber = 0;
+  currentOperator = null;
+  flagNew = true;
+}
+
+function DisplayPrevious() {
+
+  db.forEach(element => {
+    console.log(element)
+  });
+}
